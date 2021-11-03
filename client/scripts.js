914615ee104382681aa832d7a0e634917af997ec
keelyt <30781249+keelyt@users.noreply.github.com>
2021-11-02 20:48:20 -0500

    added contact form
    The "Contact" link in the header now opens a contact form rather than opening the user's mail.  The contact form does not actually work.

--- client/scripts.js
+++ client/scripts.js
@@ -1,5 +1,35 @@
-$('.menu-toggle').click(function(){
+$(".menu-toggle").click(function(){
     $(".links").toggleClass("mobile-nav");
     $(this).toggleClass("is-active");
  });
  
+
+//  Check for valid email syntax
+// function validateEmail(email) {
+//     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//     return re.test(email);
+//   }
+
+$(function() {
+  
+    // contact form animations
+    $("#contact").click(function() {
+      $("#contact-container").fadeToggle();
+    })
+
+    $("#contact-close").click(function() {
+        $("#contact-container").fadeToggle();
+      })
+      
+    $(document).mouseup(function (e) {
+      var container = $("#contact-container");
+      var form = $("#contact-form");
+  
+      if (!form.is(e.target) // if the target of the click isn't the container...
+          && form.has(e.target).length === 0) // ... nor a descendant of the container
+      {
+          container.fadeOut();
+      }
+    });
+    
+  });
